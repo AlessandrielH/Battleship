ad4ef3e85d42a5f0d9267c6bc2326a7a5de27c72
AlessandrielH <65774205+AlessandrielH@users.noreply.github.com>
2021-07-08 19:11:53 -0400

    Removed placeholder code for Ships (#41)
    I had code that was holding the place of where the ships would go. I removed that code.  There is pseudocode in its place now.

--- BattleshipProject.cpp
+++ BattleshipProject.cpp
@@ -5,7 +5,7 @@
 #include <iostream>
 #include <string>
 #include "Board.h"
-#include "Battleship.h"
+#include "Ship.h"
 
 const int COL=10;
 const int ROW=10;
@@ -14,7 +14,6 @@ using namespace std;
 
 void Attack(string, int*, int*);
 bool hitSuccess(Board, int, int);
-void setShips(Battleship, Board,int, int, int, int, int);
 bool GameOver( Board, Board);
 
 void displayMenu()
@@ -38,8 +37,8 @@ int main()
         string player1;//player1 name
         string player2;//player2 name
         int x1, x2, y1, y2;//ship placement coordinants
+
         int xCoordinate, yCoordinates;//attack coordinates
-        int size = 3;//Ship size
         bool gameOver=false;
         bool hit = false;//attack verification
         
@@ -70,21 +69,7 @@ int main()
             p1Board.displayBoard();
 
             //Place battleship for player1
-            Battleship p1ShipA;
-            cout << player1 << ", please place first battleship." << endl;
-            setShips(p1ShipA,p1Board, size, x1, y1, x2, y2);
-            //Place battleship for player1
-            cout << player1 << ", please place second battleship." << endl;
-            Battleship p1ShipB;
-            setShips(p1ShipB, p1Board, size, x1, y1, x2, y2);
-            //Place battleship for player1
-            cout << player1 << ", please place third battleship." << endl;
-            Battleship p1ShipC;
-            setShips(p1ShipC, p1Board, size, x1, y1, x2, y2);
-            //Place battleship for player1
-            cout << player1 << ", please place fourth battleship." << endl;
-            Battleship p1ShipD;
-            setShips(p1ShipD, p1Board, size, x1, y1, x2, y2);
+
 
             // builds the gameboard for player2
             cout << "Creating board for " << player2 << endl;
@@ -94,21 +79,7 @@ int main()
             p2Board.displayBoard();
 
             //Place battleship for player2
-            cout << player2 << ", please place first battleship." << endl;
-            Battleship p2ShipA;
-            setShips(p2ShipA, p2Board, size, x1, y1, x2, y2);
-            //Place battleship for player2
-            cout << player2 << ", please place second battleship." << endl;
-            Battleship p2ShipB;
-            setShips(p2ShipB, p2Board, size, x1, y1, x2, y2);
-            //Place battleship for player2
-            cout << player2 << ", please place third battleship." << endl;
-            Battleship p2ShipC;
-            setShips(p2ShipC, p2Board, size, x1, y1, x2, y2);
-            //Place battleship for player2
-            cout << player2 << ", please place fourth battleship." << endl;
-            Battleship p2ShipD;
-            setShips(p2ShipD, p2Board, size, x1, y1, x2, y2);
+
         
             do
             {
@@ -187,34 +158,6 @@ void Attack(string name, int* x, int* y)
             *y = tempY;
         }
     } while (*x==-1||*y==-1);
-}
-
-void setShips(Battleship ship,Board b,int size, int x1, int y1, int x2, int y2)
-{
-    bool error = true;
-    do
-    {
-        cout << "Enter the x-coordinant for the front of battleship: ";
-        cin >> x1;
-        cout << "Enter the y-coordinant for the front of battleship: ";
-        cin >> y1;
-        cout << "Enter the x-coordinant for the back of battleship: ";
-        cin >> x2;
-        cout << "Enter the y-coordinant for the back of battleship: ";
-        cin >> y2;
-
-        if (x1<0 || x1>b.getsize() || x2<0 || x2>b.getsize() || y1<0 || y1>b.getsize() || y2<0 || y2>b.getsize())
-        {
-            error = true;
-        }
-        else
-        {
-            error = false;
-        }
-    } while (error ==true);
-
-
-    ship.placeShip(b, size, x1, y1, x2, y2);
 }
 
 bool gameOver(Board attacker, Board victim)
