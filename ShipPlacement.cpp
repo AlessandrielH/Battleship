f8ba3270b737092f5c142f11ae59698df0704168
joyminor <86851049+joyminor@users.noreply.github.com>
2021-07-12 15:25:24 -0400

    Update ShipPlacement.cpp (#81)

--- ShipPlacement.cpp
+++ ShipPlacement.cpp
@@ -3,14 +3,26 @@
 using namespace std;
 
 	
-	void ShipPlacement::placeShips(char o, int x, int y)
+	void ShipPlacement::placeShips(Board b)
 		/* this function accepts: 3 variables to store char orientation, int x coordinate, int y coordinate.
 		No values returned.
 		Stores orientation (v for vertical, h for horizontal) that user inputs.
 		Stores x and y coordinates from user.
 		*/
 	{
-		orientation = o;
+		for (int n = 0; n < 4; n++)
+		{
+			cout << "Your " << oNos[n] << " ship is named " << names[n] << endl; //oNos is ordinal number
+			cout << "This ship is " << units[n] << " long."; //units refers to the length of the ship
+			cout << "Do you want it to be vertical or horizontal: v/h? "; // v and h refers to the orientation 
+			cin >> orientation;
+			cout << "Set the x-coordinate for the beginninng of the ship:";
+			cin >> xCoord;
+			cout << "Set the y-coordinate for the beginning of the ship:";
+			cin >> yCoord;
+			setShipPlacement(b);
+		}
+		
 		xCoord = x;
 		yCoord = y;
 		cout << orientation << " " << xCoord << " " << yCoord;
@@ -35,17 +47,5 @@ using namespace std;
 			{
 				b.setShip(xCoord, (yCoord + i)); // b refers to Board file
 			}
-		}
-	}
-	
-	void ShipPlacement::setShip(Board b)
-	{
-		for (int n = 0; n < 4; n++)
-		{
-			cout << "Your " << oNos[n] << " ship is named " << names[n] << endl; //oNos is ordinal number
-			cout << "This ship is " << units[n] << " long."; //units refers to the length of the ship
-			cout << "Do you want it to be vertical or horizontal: v/h? "; // v and h refers to the orientation 
-			cin >> orientation;
-			setShipPlacement(b);
 		}
 	}
