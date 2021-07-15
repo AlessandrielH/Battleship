83688e331ce1d54ece056fe14dd4e95a62419fc9
AlessandrielH <65774205+AlessandrielH@users.noreply.github.com>
2021-07-14 23:12:21 -0500

    Update ShipPlacement.cpp
    added iostream
    added ShipPlacement.h
    added ShipPlacement constructor
    added x, y defintions to placeship()
    Moved oNos[], names to placeShip()

--- ShipPlacement.cpp
+++ ShipPlacement.cpp
@@ -1,46 +1,46 @@
+#include"ShipPlacement.h"
 #include "Board.h"
 #include <string>
+#include<iostream>
 using namespace std;
 
-	
+ShipPlacement::ShipPlacement()
+{}
+
 	void ShipPlacement::placeShips(Board b)
-		/* this function accepts: 3 variables to store char orientation, int x coordinate, int y coordinate.
-		No values returned.
-		Stores orientation (v for vertical, h for horizontal) that user inputs.
-		Stores x and y coordinates from user.
-		*/
 	{
-		for (int n = 0; n < 4; n++)
+		int x, y;
+		string names[NUM_OF_SHIPS] = { "Conqueror", "Captain", "Victory", "Talent" }; //names of ships to be placed by players
+		string oNos[NUM_OF_SHIPS] = { "1st", "2nd", "3rd", "4th" }; //ships are numbered, respectively
+
+		for (int n = 0; n < NUM_OF_SHIPS; n++)
 		{
 			cout << "Your " << oNos[n] << " ship is named " << names[n] << endl; //oNos is ordinal number
 			cout << "This ship is " << units[n] << " long."; //units refers to the length of the ship
 			cout << "Do you want it to be vertical or horizontal: v/h? "; // v and h refers to the orientation 
 			cin >> orientation;
 			cout << "Set the x-coordinate for the beginning of the ship:";
-			cin >> xCoord;
+			cin >> x;
 			cout << "Set the y-coordinate for the beginning of the ship:";
-			cin >> yCoord;
+			cin >> y;
 			setShipPlacement(b);
 		}
 		
 		//jgminor7 added code below so user doesn't place a ship out-of-bounds of board area
 		do 
-		    {
-
-			(if xCoord < 0 || xCoord >9 || yCoord < 0 || yCoord >9  ) //const int COL = 10 //const int ROW = 10;
+		{
+			if( x < 0 || x >9 || y < 0 || y > 9) //const int COL = 10 //const int ROW = 10;
 			{
-				/*Is there a separate statement for x coordinte and y coordinate? 
+				/*Is there a separate statement for x coordinte and y coordinate?
 				Should I refer to COL and ROW, instead of xCoord and yCoord? */
-
-				cout<<"Your coordinates are out of bounds.\n"; 
-				cout<<"Please enter valid x coordinate and y coordinate within 0 to 9."<< endl;
-				cout << "Set the x-coordinate for the beginning of the ship:";
+				cout << "Your coordinates are out of bounds.\n";
+				cout << "Please enter valid x coordinate and y coordinate within 0 to 9." << endl;
+				cout << "Set the x-coordinate for the beginninng of the ship:";
 				cin >> xCoord;
 				cout << "Set the y-coordinate for the beginning of the ship:";
 				cin >> yCoord;
 			}
-		    } while(xCoord < 0 || xCoord >9 || yCoord < 0 || yCoord >9  );
-		
+		} while(x < 0 || x >9 || y < 0 || y >9  );
 		xCoord = x;
 		yCoord = y;
 		cout << orientation << " " << xCoord << " " << yCoord;
