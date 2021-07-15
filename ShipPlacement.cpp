12733fc8da2724eda63140ae560d5825ae233a79
AlessandrielH <65774205+AlessandrielH@users.noreply.github.com>
2021-07-15 16:13:30 -0400

    Update ShipPlacement.cpp (#97)
    removed setshipplacement()
    placed code from setshipplacement() into placeship()

--- ShipPlacement.cpp
+++ ShipPlacement.cpp
@@ -16,16 +16,14 @@ void ShipPlacement::placeShips(Board b)
 
 	for (int n = 0; n < NUM_OF_SHIPS; n++)
 	{
+		cout << "Your " << oNos[n] << " ship is named " << names[n] << endl; //oNos is ordinal number
+		cout << "This ship is " << units[n] << " units long."; //units refers to the length of the ship
 		do
 		{
-
-			cout << "Your " << oNos[n] << " ship is named " << names[n] << endl; //oNos is ordinal number
-			cout << "This ship is " << units[n] << " units long."; //units refers to the length of the ship
-
 			cout << "Do you want it to be vertical or horizontal: v/h? "; // v and h refers to the orientation 
 			cin >> orientation;
 		} while (orientation != 'v' && orientation != 'h');
-		
+
 		//jgminor7 added code below so user doesn't place a ship out-of-bounds of board area
 		do
 		{
@@ -42,32 +40,31 @@ void ShipPlacement::placeShips(Board b)
 		} while (x < 0 || x >9 || y < 0 || y >9);
 		xCoord = x;
 		yCoord = y;
-		cout << "orientation:"<<orientation << "\nx:" <<xCoord << "\ny:" << yCoord << endl;
-		setShipPlacement(b);
+		cout << "orientation:" << orientation << "\nx:" << xCoord << "\ny:" << yCoord << endl;
+
+
+
+		orientation = tolower(orientation);
+		if (orientation == 'v') // v is vertical ship placement, size of ship increments by 1 unit
+		{
+			for (int i = 0; i < units[i]; i++)
+			{
+				b.setShip((xCoord + i), yCoord); // b refers to Board file
+			}
+		}
+		else if (orientation == 'h') // h is horizontal ship placement, size of ship increments by 1 unit
+		{
+			for (int i = 0; i < units[i]; i++)
+			{
+				b.setShip(xCoord, (yCoord + i)); // b refers to Board file
+			}
+		}
 		b.displayBoard();
+		cout << "SetShipPLacement ended" << endl;
+
 	}
 }
 char ShipPlacement::getOrientation()
 {
 	return orientation;
-}
-
-void ShipPlacement::setShipPlacement(Board b) //  Board b refers to Board file
-{
-	orientation = tolower(orientation);
-	if (orientation == 'v') // v is vertical ship placement, size of ship increments by 1 unit
-	{
-		for (int i = 0; i < units[i]; i++)
-		{
-			b.setShip((xCoord + i), yCoord); // b refers to Board file
-		}
-	}
-	else if (orientation == 'h') // h is horizontal ship placement, size of ship increments by 1 unit
-	{
-		for (int i = 0; i < units[i]; i++)
-		{
-			b.setShip(xCoord, (yCoord + i)); // b refers to Board file
-		}
-	}
-	cout << "SetShipPLacement ended"<< endl;
 }
