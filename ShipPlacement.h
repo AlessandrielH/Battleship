80e618d1f88830dce167e6551c4eec5f0c05cf06
AlessandrielH <65774205+AlessandrielH@users.noreply.github.com>
2021-07-14 23:20:22 -0500

    Update ShipPlacement.h
    added ifndefine shipplacement
    added define shipplacement
    deleted namespace std
    removed orientation initiation
    moved oNos[], names[] to ShipPlacement.cpp
    added shipplacement constructor
    changed parameters of placeship() to Board
    deleted setShip()

--- ShipPlacement.h
+++ ShipPlacement.h
@@ -1,26 +1,26 @@
 #pragma once
 #include <iostream>
-#include<string>
-using namespace std;
+#include <string>
 #include "Board.h"
+
+#ifndef SHIPPLACEMENT
+#define SHIPPLACEMENT
 
 class ShipPlacement
 {
 	private:
-		char orientation='o'; // orientation refers to ship's position vertical or horizontal
-		int xCoord; // x coordinate
-		int yCoord; // y coordinate
 		static const int NUM_OF_SHIPS = 4;
+		char orientation; // orientation refers to ship's position vertical or horizontal
+		int xCoord; // x coordinate
+		int yCoord; // y coordinate 
 
-		string names[NUM_OF_SHIPS] = { "Conqueror", "Captain", "Victory", "Talent" }; //names of ships to be placed by players
-
-		string oNos[NUM_OF_SHIPS] = { "1st", "2nd", "3rd", "4th" }; //ships are numbered, respectively
-
-		int units[NUM_OF_SHIPS] = { 3, 4, 5, 6 }; // the length of the respective ships are 3 units, 4 units, 5 units, & 6th units
-
+		/*string names[NUM_OF_SHIPS]; //names of ships to be placed by players
+		string oNos [NUM_OF_SHIPS]; //ships are numbered, respectively*/
+		int units [NUM_OF_SHIPS]={ 3, 4, 5, 6 }; // the length of the respective ships are 3 units, 4 units, 5 units, & 6th units
 
 	public:
-		void placeShips(char o, int x, int y);
+		ShipPlacement();
+		void placeShips(Board b);
 		/* this function accepts: 3 variables to store char orientation, int x coordinate, int y coordinate.
 			   No values returned.
 			   Stores orientation (v for vertical, h for horizontal) that user inputs.
@@ -30,7 +30,6 @@ class ShipPlacement
 		void setShipPlacement(Board b);
 		//  Board b refers to Board file
 
-		void setShip(Board b);
-
 		char getOrientation();
 	};
+#endif 
