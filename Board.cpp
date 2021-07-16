1ce4a6037e5ba2db0d808b551887c8c277f96b71
AlessandrielH <65774205+AlessandrielH@users.noreply.github.com>
2021-07-16 09:48:12 -0500

    Update Board.cpp

--- Board.cpp
+++ Board.cpp
@@ -59,9 +59,11 @@ void Board::updateBoard(bool hit, int x, int y) //This function updates the boar
     if (hit==true)
     {
        board[x][y]='H'; //the letter H shows that this ship has been hit
+       cout << "You hit a battleship!!!" << endl;
     }
-    else 
+    else if(hit==false)
     {
        board[x][y]='M'; //the letter M shows that this spot is a miss because no ship was placed there
+       cout << "You missed!!! Hahaha!!!" << endl;
     }
 }
